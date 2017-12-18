55b711ad23efd7109f973d9808a8ef4825182742
Sankevich Dmitriy <dima.sankevich@gmail.com>
2017-12-18 19:16:12 +0300

    Some fixes

--- Wifi/IiPUlab6(WiFi)/Properties/AssemblyInfo.cs
+++ Wifi/IiPUlab6(WiFi)/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IIPU_lab6")]
+[assembly: AssemblyTitle("IiPUlab6(WiFi)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IIPU_lab6")]
+[assembly: AssemblyProduct("IiPUlab6(WiFi)")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b365b4e1-3f63-44ab-a84b-4891346e0b8b")]
+[assembly: Guid("6b98a450-4cd0-46b4-9f7a-c51ddbd20b43")]
 
 // Version information for an assembly consists of the following four values:
 //
